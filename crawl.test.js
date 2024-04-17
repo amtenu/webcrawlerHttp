62af4e259d446d31298dcb05feddd1b6728be15c
amtenu <amannov21@gmail.com>
2024-04-17 09:04:09 -0600

    Removed the last slash from a urlObj and test suit

--- crawl.test.js
+++ crawl.test.js
@@ -2,7 +2,7 @@ const { normalizeUrl } = require("./crawl.js");
 const { test, expect } = require('@jest/globals');
 
 test("normalizeUrl with out protocols", () => {
-  const input = "https://blogs.boot.dev/path";
+  const input = "https://blogs.boot.dev/path/";
   const actualOutput = normalizeUrl(input);
   const expected = "blogs.boot.dev/path";
   expect(actualOutput).toEqual(expected);
