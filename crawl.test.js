7c501fd18639da091598953cd3e2e633bd85e885
amtenu <amannov21@gmail.com>
2024-04-17 09:08:14 -0600

    test update

--- crawl.test.js
+++ crawl.test.js
@@ -1,7 +1,15 @@
 const { normalizeUrl } = require("./crawl.js");
-const { test, expect } = require('@jest/globals');
+const { test, expect } = require("@jest/globals");
 
-test("normalizeUrl with out protocols", () => {
+
+test("normalizeUrl without protocols", () => {
+  const input = "https://blogs.boot.dev/path";
+  const actualOutput = normalizeUrl(input);
+  const expected = "blogs.boot.dev/path";
+  expect(actualOutput).toEqual(expected);
+});
+
+test("normalizeUrl without trailing slashes", () => {
   const input = "https://blogs.boot.dev/path/";
   const actualOutput = normalizeUrl(input);
   const expected = "blogs.boot.dev/path";
